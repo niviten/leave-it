19525eb317041a4a52aa08b4b85d317556970c51
niviten <l.j.nivethan@gmail.com>
2025-05-01 21:27:22 +0530

    server/feat: config

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"leaveit/internal/config"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,9 +11,15 @@ import (
 func main() {
 	e := echo.New()
 
+	err := config.Load()
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello there")
 	})
 
-	e.Logger.Fatal(e.Start(":7000"))
+	e.Logger.Fatal(e.Start(":" + config.Get("PORT")))
 }
